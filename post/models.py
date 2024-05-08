e381713cf6b5392204bbf8fb679a1986139a7eb6
UsmanGamidov <osmangamidov026@gmail.com>
2024-05-08 14:12:27 +0300

    first commit

--- post/models.py
+++ post/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    price = models.IntegerField()
+    title = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
     discount_price = models.IntegerField(blank=True)
     remainder =  models.IntegerField(default=0)
-    characteristics = models.CharField()
+    characteristics = models.CharField(max_length=10000)
     # category =
